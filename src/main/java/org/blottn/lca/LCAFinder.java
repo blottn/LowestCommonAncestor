0f5c0262c4df143f37f1a3428d3e86756dee264e
Nicholas Blott <blottn@tcd.ie>
2017-10-25 11:51:00 +0100

    removed parent reference so it's technically a dag

--- src/main/java/org/blottn/lca/LCAFinder.java
+++ src/main/java/org/blottn/lca/LCAFinder.java
@@ -18,20 +18,18 @@ public class LCAFinder {
 	private Node[] dag;
 
 	class Node {
-		int parent;
 		int left;
 		int right;
-		public Node(int left, int right, int parent) {
+		public Node(int left, int right) {
 			this.left = left;
 			this.right = right;
-			this.parent = parent;
 		}
 	}
 
     public LCAFinder(int size, int root) {
 		dag = new Node[size];
 		for (int i = 0; i < dag.length ; i++) {
-			dag[i] = new Node(NONE, NONE, NONE);
+			dag[i] = new Node(NONE, NONE);
 		}
 		this.root = root;
     }
@@ -53,36 +51,10 @@ public class LCAFinder {
     }
 
     public void setLeft(int key, int left) {
-		if (dag[key].left != NONE) {
-			dag[dag[key].left].parent = NONE;
-		}
 		dag[key].left = left;
-		if (dag[left].parent != NONE) {
-			if (dag[dag[left].parent].left == left) {
-				dag[dag[left].parent].left = NONE;
-			}
-			if (dag[dag[left].parent].right == left) {
-				dag[dag[left].parent].right = NONE;
-			}
-		}
-
-		dag[dag[key].left].parent = key;
     }
 
     public void setRight(int key, int right) {
-		if (dag[key].right != NONE) {	//if it has a right node, set that ones parent to NONE
-			dag[dag[key].right].parent = NONE;
-		}
 		dag[key].right = right;
-		if (dag[right].parent != NONE) {
-			if (dag[dag[right].parent].right == right) {
-				dag[dag[right].parent].right = NONE;
-			}
-			if (dag[dag[right].parent].left == right) {
-				dag[dag[right].parent].left = NONE;
-			}
-		}
-
-		dag[dag[key].right].parent = key;
     }
 }
